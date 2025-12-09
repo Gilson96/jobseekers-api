7cd4f3550f501d820192425aa0b54b458f8e103c
Gilson96 <grafael99@gmail.com>
2025-12-09 15:08:03 +0000

    Add users routes

--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import companyRouter from './routes/company.js'
 import { handleCustomError, handlePsqlError, handleSeverError } from './middleware/errorHandlers.js'
+import usersRouter from './routes/users.js'
 
 const app = express()
 
@@ -10,6 +11,7 @@ const apiRouter = express.Router()
 
 app.use("/api", apiRouter)
 apiRouter.use("/company", companyRouter)
+apiRouter.use("/users", usersRouter)
 
 app.use(handlePsqlError);
 app.use(handleCustomError);
