d2928f29b35f93ba44a0f30ef950b90bcfa0c41f
Gilson96 <grafael99@gmail.com>
2025-12-15 11:06:58 +0000

    Create login controller

--- controllers/login.ts
+++ controllers/login.ts
@@ -8,34 +8,40 @@ export const login = (req: Request, res: Response) => {
     const { email }: { email: string } = req.body
     const { password }: { password: string } = req.body
 
-    return checkIfExists('users', 'email', email).then((result) => {
-        if (!result) {
-            return res.status(404).send({ msg: 'User email not found' })
+    return Promise.all([
+        checkIfExists('users', 'email', email),
+        checkIfExists('company', 'email', email)
+    ]).then((result) => {
+        if (!result[0] && !result[1]) {
+            return res.status(404).send({ msg: 'Email not found' })
         } else {
             findOne(undefined, email).then((user) => {
-                const userPassword = user[0]?.password!
+                const user_password = user[0]?.password!
                 const user_id = user[0]?.user_id!
+                const user_role = user[0]?.role!
 
-                bcrypt.compare(password, userPassword, (err, result) => {
+                bcrypt.compare(password, user_password, (err, result) => {
                     if (err) {
-                        console.log('Error comparing passowrds:', err)
+                        return res.status(400).send({ msg: 'Error comparing passowrds:', err })
                     } else {
                         if (result) {
                             const token = jwt.sign(
-                                { user_id: user_id, email: email },
+                                { user_id: user_id, email: email, role: user_role },
                                 'c_cret_password',
                                 {
                                     expiresIn: '1h',
                                     algorithm: 'HS256'
+
                                 }
                             )
                             return res.status(200).send({
                                 user_id: user[0]?.user_id,
                                 email: user[0]?.email,
+                                role: user[0]?.role,
                                 token: token
                             })
                         } else {
-                            return res.status(401).send({ msg: "Incorrect password" })
+                            return res.status(401).send({ msg: "Wrong password" })
                         }
                     }
                 })
