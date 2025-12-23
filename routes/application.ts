5a19572ff587de333db14b0cd806e20a66d6bfeb
Gilson96 <grafael99@gmail.com>
2025-12-23 10:00:52 +0000

    Debugging CORS

--- routes/application.ts
+++ routes/application.ts
@@ -2,10 +2,9 @@ import express from 'express'
 import { createApplication } from '../controllers/application.js'
 import multer from 'multer'
 
-const cvUpload = multer({ dest: '../cv_uploads' })
 
 const applicationRouter = express.Router()
 
-applicationRouter.post("/", cvUpload.single('cv'), createApplication)
+applicationRouter.post("/",  createApplication)
 
 export default applicationRouter
