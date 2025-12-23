7670a916b865bab96da9492392e895d92bb7f7f9
Gilson96 <grafael99@gmail.com>
2025-12-23 08:46:08 +0000

    Add OPTIONS method to cors

--- app.ts
+++ app.ts
@@ -16,11 +16,12 @@ import { login } from './controllers/login.js'
 const app = express()
 
 app.use(cors({ 
-    origin: 'http://localhost:5173/', 
+    origin: 'http://localhost:5173', 
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"], 
 }));
+app.options("*", cors());
 
 app.use(express.json())
 
