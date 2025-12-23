1cccbd3d35b4a923412935ffa1026b3b0ad90698
Gilson96 <grafael99@gmail.com>
2025-12-23 10:03:47 +0000

    Debugging CORS

--- routes/application.ts
+++ routes/application.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import { createApplication } from '../controllers/application.js'
-import multer from 'multer'
 
 
 const applicationRouter = express.Router()
--- routes/applicationJob.ts
+++ routes/applicationJob.ts
@@ -1,12 +1,10 @@
 import express from 'express'
 import { createApplicationJob, findAllApplicationJob, uploadCVToApplication } from '../controllers/applicationJob.js'
 import { checkAuth } from '../middleware/auth.js'
-import multer from 'multer'
 
-const cvUpload = multer({ dest: '../cv_uploads' })
 const applicationJobRouter = express.Router()
 
-applicationJobRouter.post('/cv', cvUpload.single('cv'), uploadCVToApplication)
+applicationJobRouter.post('/cv', uploadCVToApplication)
 
 applicationJobRouter.post("/", createApplicationJob)
 applicationJobRouter.get("/:job_id", checkAuth, findAllApplicationJob)
