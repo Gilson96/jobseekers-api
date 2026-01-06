d92b619097861aacfde422a34d731e464b7fd449
Gilson96 <grafael99@gmail.com>
2026-01-06 11:21:53 +0000

    Resetting database

--- data/skills_user.ts
+++ data/skills_user.ts
@@ -1,22 +1,22 @@
 export const skills_user = [
     {
-        user_id: 1,
+        user_id: 2,
         skills_id: 4,
     },
     {
-        user_id: 1,
+        user_id: 2,
         skills_id: 20,
     },
     {
-        user_id: 1,
+        user_id: 2,
         skills_id: 12,
     },
     {
-        user_id: 1,
+        user_id: 2,
         skills_id: 17,
     },
     {
-        user_id: 1,
+        user_id: 2,
         skills_id: 15,
     },
 ]
